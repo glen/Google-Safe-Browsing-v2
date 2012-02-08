72d5bb2b97000986cd08c085346459a23159d4e4
Glen Noronha <glen.noronha@gmail.com>
2012-02-08 21:57:37 +0530

    Updated the url_checker to work within Ruby 1.8.7 and 1.9.2

--- test/url_checker.rb
+++ test/url_checker.rb
@@ -1,17 +1,29 @@
 require 'rubygems'
-require 'ruby-debug'
 require 'uri'
 require 'net/http'
 require 'time'
 require 'redis'
 require 'digest'
 require 'iconv'
+require 'yaml'
+
+# File used to read the domains, I am using Alexa's 1 million domains list
+URL_FILE = "top-1m.csv"
 
 CONFIG = YAML.load(File.read(File.expand_path(File.join('config', 'config.yml'))))["development"]
-
-class Hash
-  def method_missing(method_name, *args, &block)
-    self[method_name.to_s]
+# We are putting the config.yml file into CONFIG and using it as CONFIG.key.
+# This is all done using daemon-kit. 
+# In lieu of daemon kit we would need to still access the methods similarly
+# Hence the code below.
+# Only for Ruby 1.9.2 for 1.8.7  use code further down
+if RUBY_VERSION == "1.9.2"
+  CONFIG.each do |key, value|
+    CONFIG.define_singleton_method(key.to_sym){value}
+  end
+else
+# For Ruby 1.8.7 use the following code
+  CONFIG.each do |key, value|
+    Hash.send(:define_method, key.to_sym, proc{value})
   end
 end
 
@@ -20,9 +32,9 @@ end
 end
 require File.expand_path(File.join('lib', 'update_list.rb'))
 
-URL_FILE = "top-1m.csv"
+all_urls = []
 
-all_urls = ["http://gumblar.cn"]
+# Opening the Alexa 1 million domain file, read each entry and put into an array.
 #File.open(URL_FILE) do |file|
 # until file.eof?
 #    line = file.readline
@@ -30,6 +42,9 @@ all_urls = ["http://gumblar.cn"]
 #    all_urls << "http://#{domain}"
 #  end
 #end
+
+# Else we can add the urlss that we need to check in the array as below
+all_urls = ["http://gumblar.cn"]
 
 puts "Starting @ #{Time.now}"
 all_urls.each do |url|
